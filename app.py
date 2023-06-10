07579bdb6efe95f19407aa7232351c64758a729b
ohsyith <mnasyith2006@gmail.com>
2023-06-10 10:25:25 +0700

    done

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+
+connection_string = 'mongodb+srv://nasyith:220306@cluster0.ssokp1a.mongodb.net/?retryWrites=true&w=majority'
+client = MongoClient(connection_string)
+db = client.dbsparta
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/diary', methods=['GET'])
+def show_diary():
+
+      articles = list(db.diary.find({}, {'_id': False}))
+      return jsonify({'articles': articles})
+      
+@app.route('/diary', methods=['POST'])
+def save_diary():
+     # sample_receive = request.form.get('sample_give')
+     # print(sample_receive)
+     title_receive = request.form.get('title_give')
+     content_receive = request.form.get('content_give')
+     doc = {
+          'title': title_receive,
+          'content': content_receive
+     }
+     db.diary.insert_one(doc)
+     return jsonify({'message': 'data saved'})
+
+if __name__ == '__main__':
+	app.run('0.0.0.0', port=5000, debug=True)
